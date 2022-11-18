7dba0a114bb68c1546ac43f2f743520b806ba837
Armin Sadeghi <sadeghiy.armin@gmail.com>
2022-11-18 14:54:58 -0500

    sync the tree seawrch code

--- src/python/infogain/infogain/policies/tree_search.py
+++ src/python/infogain/infogain/policies/tree_search.py
@@ -8,16 +8,86 @@
 '''
 
 
-from policies.random import RandomPolicy
+from policies.random_policy import RandomPolicy
+from tree_search_util import DecisionTree, Node, Action
+from queue import Queue
+from policies.VelocityGrid import VelocityGrid
+from config import *
 
-
-
-class TreeSearch(RandomPolicy):
+class TreeSearchPolicy(RandomPolicy):
     def __init__(self, generator, policy_args=None) -> None:
         super().__init__(policy_args)
+        self.action_set = policy_args['action_set']
         
+
+    def _get_next_state(self, current_state : list, action : list):
+        """returns the next state of the ego vehicle after taking action
+
+        Args:
+            current_state (list): the current location and orientation of the ego vehicle
+            action (list): the speed input
+        """
+        # for now I assume simple speed input for the ego vehicle
+        # TODO: maybe better if we define the action set with a set of accelerations 
+        
+        return [
+            current_state[0] + action[0] * TICK_TIME,
+            current_state[1] + action[1] * TICK_TIME
+        ]
+        
+        
+    def create_decision_tree(self, ego, velocity_grids):
+        """
+        create the decision tree given the velocity grids
+        """
+        max_tree_depth = len(velocity_grids)
+        
+        current_state = [
+            ego.pos[0], ego.pos[1]
+        ]
+        
+        tree = DecisionTree()
+        root = Node (parent = -1, idx = 0, prob = 1, state=current_state)
+        
+        tree.add_node(root)
+        visited = Queue()
+        visited.put(root)
+        
+        tree_depth = 0
+        
+        
+        while visited.qsize() > 0:
+            current_node = visited.pop(0)
+            for action in self.action_set:
+                # get the future state of the ego vehicle after taking 
+                # action 
+                future_state = self._get_next_state(
+                    current_node.state,
+                    action
+                )
+                
+                # TODO: find the visibility for the future state
+                
+                
+                node = Node(
+                    current_node.idx, 
+                    tree.num_nodes, 
+                    
+                    )
+                tree.add_node(node)
+
+            tree_depth += 1
+            if tree_depth > max_tree_depth:
+                break
     
-    def execute(self, ego, actors, current_time=0, max_solver_time=30, dt=0.1):
+    def execute(
+            self, 
+            ego, 
+            actors,
+            velocity_grids,
+            visibility=None,
+            current_time=0, 
+            max_solver_time=30, dt=0.1):
         """tree search policy
 
         Args:
@@ -27,7 +97,7 @@ class TreeSearch(RandomPolicy):
             max_solver_time (int, optional): _description_. Defaults to 30.
             dt (float, optional): _description_. Defaults to 0.1.
         """
-        
+        self.create_decision_tree(ego, velocity_grids)       
         
         
         
