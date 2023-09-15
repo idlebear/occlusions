4e6c20d65f05fe1464998c495309e54dc016c6b8
Idlebear <idlebear@redmaples.ca>
2023-09-15 11:36:24 -0400

    wip:single run

--- src/python/infogain/infogain/run_experiment.sh
+++ src/python/infogain/infogain/run_experiment.sh
@@ -14,16 +14,30 @@ then
     shift
 fi
 
+echo "1"
+python run_gym.py --prefix noforget-oneside --actors $actors --method mppi --samples $samples --horizon $horizon --visibility-cost Ours --seed 6893 --simulation-steps $simulation_steps --runs $runs  > /dev/null 2>&1 &
+python run_gym.py --prefix noforget-oneside --actors $actors --method mppi --samples $samples --horizon $horizon --visibility-cost Ours --seed 42 --simulation-steps $simulation_steps --runs $runs  > /dev/null 2>&1 &
+python run_gym.py --prefix noforget-oneside --actors $actors --method mppi --samples $samples --horizon $horizon --visibility-cost Ours --seed 520 --simulation-steps $simulation_steps --runs $runs  > /dev/null 2>&1 &
+python run_gym.py --prefix noforget-oneside --actors $actors --method mppi --samples $samples --horizon $horizon --visibility-cost Ours --seed 97 --simulation-steps $simulation_steps --runs $runs  > /dev/null 2>&1 &
+python run_gym.py --prefix noforget-oneside --actors $actors --method mppi --samples $samples --horizon $horizon --visibility-cost Ours --seed 29348 --simulation-steps $simulation_steps --runs $runs  > /dev/null 2>&1 &
+wait
+echo "2"
+python run_gym.py --prefix noforget-oneside --actors $actors --method mppi --samples $samples --horizon $horizon --visibility-cost Ours --seed 935567 --simulation-steps $simulation_steps --runs $runs  > /dev/null 2>&1 &
+python run_gym.py --prefix noforget-oneside --actors $actors --method mppi --samples $samples --horizon $horizon --visibility-cost Ours --seed 7 --simulation-steps $simulation_steps --runs $runs  > /dev/null 2>&1 &
+python run_gym.py --prefix noforget-oneside --actors $actors --method mppi --samples $samples --horizon $horizon --visibility-cost Ours --seed 4913 --simulation-steps $simulation_steps --runs $runs  > /dev/null 2>&1 &
+python run_gym.py --prefix noforget-oneside --actors $actors --method mppi --samples $samples --horizon $horizon --visibility-cost Ours --seed 84832 --simulation-steps $simulation_steps --runs $runs  > /dev/null 2>&1 &
+python run_gym.py --prefix noforget-oneside --actors $actors --method mppi --samples $samples --horizon $horizon --visibility-cost Ours --seed 5566 --simulation-steps $simulation_steps --runs $runs  > /dev/null 2>&1 &
+wait
 
-# while (( "$#" )); do
-    # l=$1
-    for s in ${seeds[*]}; do
-        echo ...seed $s
-        python run_gym.py --actors $actors --method mppi --samples $samples --horizon $horizon --visibility-cost Nominal --seed $s --simulation-steps $simulation_steps --runs $runs  > /dev/null 2>&1 &
-        python run_gym.py --actors $actors --method mppi --samples $samples --horizon $horizon --visibility-cost None --seed $s --simulation-steps $simulation_steps --runs $runs > /dev/null 2>&1 &
-        python run_gym.py --actors $actors --method mppi --samples $samples --horizon $horizon --visibility-cost Higgins --seed $s --simulation-steps $simulation_steps --runs $runs > /dev/null 2>&1 &
-        python run_gym.py --actors $actors --method mppi --samples $samples --horizon $horizon --visibility-cost Ours --seed $s --simulation-steps $simulation_steps --runs $runs > /dev/null 2>&1 &
-        wait
-    done
-    # shift
-# done
+# # while (( "$#" )); do
+#     # l=$1
+#     for s in ${seeds[*]}; do
+#         echo ...seed $s
+#         python run_gym.py --prefix single-oneside --actors $actors --method mppi --samples $samples --horizon $horizon --visibility-cost Nominal --seed $s --simulation-steps $simulation_steps --runs $runs  > /dev/null 2>&1 &
+#         python run_gym.py --prefix single-oneside --actors $actors --method mppi --samples $samples --horizon $horizon --visibility-cost None --seed $s --simulation-steps $simulation_steps --runs $runs > /dev/null 2>&1 &
+#         python run_gym.py --prefix single-oneside --actors $actors --method mppi --samples $samples --horizon $horizon --visibility-cost Higgins --seed $s --simulation-steps $simulation_steps --runs $runs > /dev/null 2>&1 &
+#         python run_gym.py --prefix single-oneside --actors $actors --method mppi --samples $samples --horizon $horizon --visibility-cost Ours --seed $s --simulation-steps $simulation_steps --runs $runs > /dev/null 2>&1 &
+#         wait
+#     done
+#     # shift
+# # done
